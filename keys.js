bb74081f2d6aa9973716a9ba14ac85f976cef85e
jmckella <jmckella@live.unc.edu>
2016-09-27 18:18:12 -0400

    Liri Node

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('Twitter Key');
+
+module.exports.twitterKeys = {
+  consumer_key: 'pnzcaV3xybWfqCQkjhfXk6tgo',
+  consumer_secret: 'iSqJs9wvcY5oK4UZx4IdZqkdRANMGtJCMJtYWU0X9a3xypMvpp',
+  access_token_key: '780833930811363328-sWQDcW4RYi4EfFOTUHlnvFVTHKpaYKM',
+  access_token_secret: '3lc3IZxr08Gb3mCjzmMFxA4s2T3y9tgTXU9vMp8ckbB4V',
+}
---
+++ liri.js
@@ -0,0 +1,6 @@
+console.log("We are getting started!");
+
+var twitter = require('twitter');
+
+var APIkeys = require('./keys');
+console.log(APIkeys);
